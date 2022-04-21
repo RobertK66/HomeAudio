7d16410b59c8d3a3820c49dc0a1b2440fef90656
RobertK66 <RobertK66@users.noreply.github.com>
2022-04-21 08:16:38 +0200

    cleanup

--- QueueCaster/ChromecastClient.cs
+++ QueueCaster/ChromecastClient.cs
@@ -13,8 +13,7 @@ using System.Threading.Tasks;
 namespace QueueCaster {
     public class ChromecastClient :Sharpcaster.ChromecastClient {
 
-
-        public ChromecastClient(IServiceCollection serviceCollection) :base(serviceCollection){
+        public ChromecastClient(IServiceCollection serviceCollection) : base(serviceCollection) {
         }
 
         public static ChromecastClient CreateNewChromecastClient() {
@@ -23,24 +22,40 @@ namespace QueueCaster {
             serviceCollection.AddTransient<IChromecastChannel, HeartbeatChannel>();
             serviceCollection.AddTransient<IChromecastChannel, ReceiverChannel>();
             serviceCollection.AddTransient<IChromecastChannel, QueueMediaChannel>();
+
+            var customMessages = new List<Assembly>();
+            customMessages.Add(typeof(QueueItem).GetTypeInfo().Assembly);
+
+            List<Type> messageImplTypes = ListAllMessageClasses(customMessages);
+
+            foreach (var item in messageImplTypes) {
+                serviceCollection.AddTransient(typeof(IMessage), item);
+                Console.WriteLine("***** " + item.FullName);
+            }
+            return new ChromecastClient(serviceCollection);
+        }
+
+        private static List<Type> ListAllMessageClasses(List<Assembly> messageAssemblies) {
+            Dictionary<string, Type> allMessagesDictionary = new();
+
+            // add the base library in front of custom assemblies -> last one wins!
+            messageAssemblies.Insert(0, typeof(IConnectionChannel).GetTypeInfo().Assembly);
+
             var messageInterfaceType = typeof(IMessage);
-            foreach (var type in (from t in typeof(IConnectionChannel).GetTypeInfo().Assembly.GetTypes()
-                                  where t.GetTypeInfo().IsClass && !t.GetTypeInfo().IsAbstract && messageInterfaceType.IsAssignableFrom(t) && t.GetTypeInfo().GetCustomAttribute<ReceptionMessageAttribute>() != null
-                                  select t)) {
-                if (!type.Name.Equals("MediaStatusMessage")) {  // Skip this Message. We replace it with our Version including queue info
-                    serviceCollection.AddTransient(messageInterfaceType, type);
-                    Console.WriteLine("***** " + type.Name);
+            foreach (Assembly assembly in messageAssemblies) {
+                foreach (var type in (from t in assembly.GetTypes()
+                                      where t.GetTypeInfo().IsClass && !t.GetTypeInfo().IsAbstract && messageInterfaceType.IsAssignableFrom(t) && t.GetTypeInfo().GetCustomAttribute<ReceptionMessageAttribute>() != null
+                                      select t)) {
+                    if (allMessagesDictionary.ContainsKey(type.Name)) {
+                        // Overwrite the base implementation with new one
+                        allMessagesDictionary.Remove(type.Name);
+                    }
+                    allMessagesDictionary.Add(type.Name, type);
                 }
             }
 
-            // Add our own Message Classes handling the queued item lists in MediaStatusMessage and others
-            foreach (var type in (from t in typeof(QueueItem).GetTypeInfo().Assembly.GetTypes()
-                                  where t.GetTypeInfo().IsClass && !t.GetTypeInfo().IsAbstract && messageInterfaceType.IsAssignableFrom(t) && t.GetTypeInfo().GetCustomAttribute<ReceptionMessageAttribute>() != null
-                                  select t)) {
-                serviceCollection.AddTransient(messageInterfaceType, type);
-                Console.WriteLine("+++++ " + type.Name);
-            }
-            return new ChromecastClient(serviceCollection);
+            return allMessagesDictionary.Values.ToList();
+            
         }
     }
 }
