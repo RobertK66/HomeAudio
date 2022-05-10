1e6e0bca2f2e84c85c6450c32ebbc71298ed9c3b
RobertK66 <RobertK66@users.noreply.github.com>
2022-05-10 12:50:34 +0200

    remove own LoadMessage

--- QueueCaster/ConsoleWrapper.cs
+++ QueueCaster/ConsoleWrapper.cs
@@ -6,10 +6,12 @@ namespace QueueCaster {
         Action<string> Writeline1 = (line) => Console.WriteLine("QueueCaster: " + line);
         Action<string, Exception, object> Writeline2 = (line, ex, p) => { };
 
-        public ConsoleWrapper(Action<string> wl1, Action<string, Exception, object> wl2) {
-            Writeline1 = wl1;
-            Writeline2 = wl2;   
+        public ConsoleWrapper(Action<string>? wl1 = null, Action<string, Exception, object>? wl2 = null) {
+            Writeline1 = wl1 ?? ((line) => Console.WriteLine("QueueCaster: " + line)); 
+            Writeline2 = wl2 ?? ((line, ex, p) => { });   
         }
+
+        
 
         public void WriteLine(string line) {
             Writeline1(line);
