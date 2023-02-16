d2c1cb100d23e0e76c897a3b49e364850d5b394c
RobertK66 <RobertK66@users.noreply.github.com>
2023-02-16 14:10:40 +0100

    radio station select funkt einmal

--- ConGui/Program.cs
+++ ConGui/Program.cs
@@ -15,8 +15,11 @@ using static System.Net.Mime.MediaTypeNames;
 using System.Threading.Tasks;
 using System.Threading;
 using System;
+using System.Linq;
+using ConsoleGUI.Common;
+using System.Collections.Generic;
 
-public class Program : IHostedService {
+public class Program : IHostedService, IInputListener {
 
     private readonly ILogger<Program> _logger;
     private IConfiguration WebRadios;
@@ -24,8 +27,9 @@ public class Program : IHostedService {
     public static async Task Main(string[] args) {
         IHostBuilder host = Host.CreateDefaultBuilder(args)
                                 .ConfigureServices((hostContext, services) => {
+                                    services.AddSingleton<CCStarter>();
+                                    //AddHostedService<CCStarter>();
                                     services.AddHostedService<Program>();
-                                    services.AddHostedService<CCStarter>();
                                 })
                                 .ConfigureLogging((cl) => {
                                     cl.ClearProviders();                // This avoids logging output to console.
@@ -37,10 +41,12 @@ public class Program : IHostedService {
         await host.RunConsoleAsync();
     }
 
-    public Program(IConfiguration conf, ILogger<Program> logger) {
+
+    private CCStarter myCC;
+    public Program(IConfiguration conf, ILogger<Program> logger, CCStarter cc) {
         _logger = logger;
         _logger.LogDebug("*********************** Program() Constructor called.");
-
+        myCC = cc;
         WebRadios = conf.GetSection("WebRadio");
     }
         
@@ -58,6 +64,32 @@ public class Program : IHostedService {
     private TextBox myTextBox = new();
     private TabPanel tabPanel = new();
 
+    private int selected = -1;
+    private int count = 0;
+    List<Background> stations = new List<Background>();
+    
+
+    public void OnInput(InputEvent inputEvent) {
+        if (inputEvent.Key.Key == ConsoleKey.Enter) {
+            _logger.LogDebug("Play selected: " + WebRadios.GetChildren().ToList()[selected].GetValue<String>("StationName"));
+            _ = myCC.PlayLive(WebRadios.GetChildren().ToList()[selected].GetValue<String>("ContentUrl"));
+        
+        } else if (selected >= 0) {
+            Background old = stations[selected];
+            if ((inputEvent.Key.Key == ConsoleKey.LeftArrow)) {
+                selected -= 1;
+                if (selected < 0) { selected = count-1; }
+            } else if ((inputEvent.Key.Key == ConsoleKey.RightArrow)) {
+                selected += 1;
+                if (selected > count-1) { selected = 0; }
+            }
+
+            Background newStat = stations[selected];
+            old.Color = new Color(22, 22, 22);
+            newStat.Color = new Color(100, 0, 0);
+        }
+    }
+
     public Task StartAsync(CancellationToken cancellationToken) {
         _logger.LogInformation("Program.StartAsync() called.");
 
@@ -65,10 +97,12 @@ public class Program : IHostedService {
         ConsoleManager.Resize(new Size(150, 40));
         ConsoleManager.Content = CreateMainView();
 
+
         input = new IInputListener[] {
                 myLogPanel,
-                myTextBox,
-                tabPanel
+                //myTextBox,
+                tabPanel,
+                this
             };
 
         //MouseHandler.Initialize();
@@ -81,7 +115,8 @@ public class Program : IHostedService {
         //var waitForCaster = ccs.Connect();
 
         _logger.LogInformation("Program.StartAsync() finished.");
-        return Task.CompletedTask;
+        //return Task.CompletedTask;
+        return myCC.StartAsync(cancellationToken);
     }
 
     public Task StopAsync(CancellationToken cancellationToken) {
@@ -91,9 +126,6 @@ public class Program : IHostedService {
         }
         return Task.CompletedTask;
     }
-
-
-   
 
 
     private void TuiThread() {
@@ -109,29 +141,31 @@ public class Program : IHostedService {
         }
     }
 
-
-
-    
     private IControl CreateMainView() {
         var myTxtBlock = new TextBlock { Text = "Hello world" };
         myTextBox = new TextBox { Text = "Hello console" };
 
-
         var pan = new ConsoleGUI.Controls.Canvas();
         int l = 0;
         int t = 10;
-        foreach (var item in WebRadios.GetChildren()) {
-            _logger.LogDebug(item.GetValue<String>("ContentUrl"));
-            var b = new Button { Content = new Button() { Content = new TextBlock() { Text = item.GetValue<String>("StationName") }, MouseOverColor = new Color(5,0,0) } };
-            pan.Add(b, new Rect(l, t, 20, 1));
-            l += 21;
-            
+        
+        count = WebRadios.GetChildren().Count();
+        if (count > 0) {
+            foreach (var item in WebRadios.GetChildren()) {
+                _logger.LogDebug(item.GetValue<String>("ContentUrl"));
+                Color backgrdcol = new Color(22, 22, 22);
+                var b = new Background { Content = new TextBlock() { Text = item.GetValue<String>("StationName") }, Color = backgrdcol };
+                stations.Add(b);
+                pan.Add(b, new Rect(l, t, 20, 1));
+                l += 21;
+            }
+            selected = 0;
+            stations[0].Color = new Color(100, 0, 0); 
         }
+
 
         //tabPanel = new TabPanel();
         tabPanel.AddTab("Radio Stations", pan);
-
-        
     
 
         tabPanel.AddTab("My Cd Collection", new Box {
@@ -163,6 +197,6 @@ public class Program : IHostedService {
 
     }
 
-
+ 
 }
 
