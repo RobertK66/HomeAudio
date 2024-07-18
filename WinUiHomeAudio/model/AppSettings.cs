33a40af090b8fad645ed2328a320cf7668d7c93f
RobertK66 <RobertK66@users.noreply.github.com>
2024-07-18 11:07:48 +0200

    more cleanup - nuget vulnerabilities

--- WinUiHomeAudio/model/AppSettings.cs
+++ WinUiHomeAudio/model/AppSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace WinUiHomeAudio.model {
     public class AppSettings {
-        public string? AutoConnectName { get; internal set; } = "Büro";//"Wohnzimmer"; //"Büro";
-        public string AppId { get; internal set; } = "9B5A75B4";
+        public string? AutoConnectName { get; internal set; } = "My JBL";//"Wohnzimmer"; //"Büro";
+        public string AppId { get; internal set; } = "46C1A819";
     }
 }
